5b74bf4539f369c3c472952e59bd6b6d3878445c
Sven Fischer <svenfischer@Sven-MacBook-Air.local>
2023-04-06 19:42:44 +0200

    changes in ejs

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use(express.static("public"));
 const starting_content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/public/html/home.html")
+    // res.sendFile(__dirname + "/public/html/home.html")
+    res.render("home")
 })
 
 
