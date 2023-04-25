bbfaa78035e80b8a9ecf0e5b88d6614247c31bf8
SvenFischer28 <81426840+SvenFischer28@users.noreply.github.com>
2023-04-25 19:29:02 +0200

    rekuperacie v2

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use("produkty", express.static(__dirname + ("public/produkty")))
 
 
 const starting_content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
@@ -18,39 +19,19 @@ app.get("/", function (req, res) {
 
 
 
-const rekuperacie = [
-    {
-        name: "rekuperacia1",
-        link: "https://www.comair.sk/index.php/produkty",
-        text: "This is the first rekuperacia object",
-        image: "/imgs/rekuperacia_o3ozon_1.jpeg"
 
-    },
-    {
-        name: "rekuperacia2",
-        price: 200,
-        text: "This is the second rekuperacia object"
-    },
-    {
-        name: "rekuperacia3",
-        price: 300,
-        text: "This is the third rekuperacia object"
-    },
-    {
-        name: "rekuperacia4",
-        price: 400,
-        text: "This is the fourth rekuperacia object"
-    }
-];
-
+let rekuperacie = require(__dirname + "/public/produkty/rekuperacie_comair.js");
 
 app.get("/rekuperacie", function (req, res) {
 
     res.render("rekuperacie/rekuperacie", { rekuperacie: rekuperacie })
 })
 
-app.get("/rekuperacie/COMAIR/rekuperacia1", function (req, res) {
-    res.render("rekuperacie/rekuperacia.ejs")
+app.get("/rekuperacie/comair/:name", function (req, res) {
+    let nazovRekuperacie = _.upperFirst([req.params.name]);
+    let rekuperacia = rekuperacie.find(item => item.url === nazovRekuperacie);
+
+    res.render("rekuperacie/rekuperacia", { rekuperacia: rekuperacia })
 })
 
 
