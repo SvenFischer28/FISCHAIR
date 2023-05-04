fff2dd5778fa9464f90bd03d9a76d055a8663a96
SvenFischer28 <81426840+SvenFischer28@users.noreply.github.com>
2023-05-04 20:06:11 +0200

    contact form nodemailer v0

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
 const app = express();
+const nodemailer = require("node-mailer")
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
