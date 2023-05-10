b03b74266b12ca4a887aad5a07d065913676c7c8
SvenFischer28 <81426840+SvenFischer28@users.noreply.github.com>
2023-05-10 20:31:45 +0200

    10.05
    working contact form, routing and repaired navbar

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const fs = require('fs');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
 const app = express();
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
+const { find } = require("lodash");
 const PORT = process.env.PORT || 3000
 app.set('view engine', 'ejs');
 
@@ -12,18 +14,11 @@ app.use(express.static("public"));
 app.use("produkty", express.static(__dirname + ("public/produkty")))
 
 
-const starting_content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 app.get("/", function (req, res) {
     // res.sendFile(__dirname + "/public/html/home.html")
     res.render("home")
 })
-
-
-
-
-
-
 
 
 app.post("/", (req, res) => {
@@ -59,35 +54,45 @@ app.post("/", (req, res) => {
 })
 
 
-let produkty = require(__dirname + "/public/produkty/produkty.js");
-let rekuperacie = produkty.rekuperacie;
 
-app.get("/rekuperacie", function (req, res) {
 
-    res.render("produkty_views/produkty", { rekuperacie: rekuperacie })
-})
+// products require
+const { rekuperacie } = require(__dirname + "/public/produkty/produkty.js");
+//////////////////////////////////
+
 
 
 app.get("/:produkty/:vyrobca", function (req, res) {
     const produktyParam = req.params.produkty;
     const vyrobca = req.params.vyrobca;
-    const renderProducts = produkty.produktyParam;
-    const renderProductsVyrobca = renderProducts.find(item => item.brand == vyrobca);
 
-
-    res.render("produkty/produkty", { produkty: renderProductsVyrobca })
+    if (produktyParam === "rekuperacie") {
+        const renderProductsVyrobca = rekuperacie.filter(obj => obj.brand === vyrobca)
+        res.render("produkty_views/produkty", { produkty: renderProductsVyrobca })
+    }
 
 
 })
 
-app.get("/rekuperacie/comair/:name", function (req, res) {
-    let nazovRekuperacie = _.upperFirst([req.params.name]);
-    let rekuperacia = rekuperacie.find(item => item.name === nazovRekuperacie);
+// app.get("/rekuperacie/comair/:name", function (req, res) {
+//     let nazovRekuperacie = _.upperFirst([req.params.name]);
+//     let rekuperacia = rekuperacie.find(item => item.name === nazovRekuperacie);
 
-    res.render("rekuperacie/rekuperacia", { rekuperacia: rekuperacia })
+//     res.render("produkty_views/rekuperacia", { produkt: rekuperacia })
+// })
+
+app.get("/produkty/:kategoriaP/:vyrobca/:produkt", (req, res) => {
+    const kategoriaP = req.params.produkt;
+    const produktP = req.params.produkt;
+
+    if (kategoriaP == "rekuperacie") {
+        let produkt = rekuperacie.find(item => item.name === produktP);
+        res.render("produkty_views/produkt", { produkt: produkt })
+    } else {
+        let produkt = rekuperacie.find(item => item.name === produktP);
+        res.render("produkty_views/produkt", { produkt: produkt })
+    }
 })
-
-
 
 
 
