6356e5ba87aee4ff4ccdfb121b3ee52b0aa5c635
SvenFischer28 <81426840+SvenFischer28@users.noreply.github.com>
2023-08-22 20:15:50 +0200

    Rekuperacie dokoncene

--- src/routes/produktRoute.js
+++ src/routes/produktRoute.js
@@ -1,22 +1,27 @@
-import express from 'express';
-import Product from '../models/products.js';
+import express from "express";
+import Product from "../models/products.js";
+import Rekuperacia from "../models/recuperationModel.js";
 const router = express.Router();
 
-router.get("/produkty/:kategoriaP/:vyrobca/:produkt", async(req, res) => {
-    const kategoriaP = req.params.produkt;
-    const produktP = req.params.produkt;
+router.get("/produkty/:kategoriaP/:vyrobca/:produkt", async (req, res) => {
+  const kategoriaP = req.params.produkt;
+  const produktP = req.params.produkt;
 
-    try {
-     const produktRender = await Product.findOne({name: produktP});
-     res.render("produkty_views/produkt", { produkt: produktRender });
+  const findAndRenderRekuperacia = async () => {
+    const rekuperacia = await Rekuperacia.findOne({ name: produktP });
+    res.render("produkty_views/rekuperacia", { rekuperacia: rekuperacia });
+  };
 
+  try {
+    // if (kategoriaP === "rekuperacie") {
+    findAndRenderRekuperacia();
+    // } else {
+    //   const produktRender = await Product.findOne({ name: produktP });
+    //   res.render("produkty_views/produkt", { produkt: produktRender });
+    // }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-    } catch (error) {
-        console.log(error);
-    }
-
-  
-})
-
-
-export {router};
+export { router };
